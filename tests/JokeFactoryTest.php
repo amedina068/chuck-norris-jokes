33d0dc229c8c4a61a2038c53c1ab1a918bc48749
Alejandro Medina <53201365+amedina068@users.noreply.github.com>
2020-02-13 20:46:23 -0600

    Apply fixes from StyleCI (#1)

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -13,7 +13,7 @@ class JokeFactoryTest extends TestCase
     public function it_returns_a_random_joke()
     {
         $jokes = new JokeFactory(['This is a joke']);
-        
+
         $joke = $jokes->getRandomJoke();
 
         $this->assertSame('This is a joke', $joke);
@@ -27,11 +27,11 @@ class JokeFactoryTest extends TestCase
         $chuckNorrisJokes = [
             'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
             'Chuck Norris counted to infinity... Twice.',
-            'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death.'
+            'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death.',
         ];
 
         $jokes = new JokeFactory;
-        
+
         $joke = $jokes->getRandomJoke();
 
         $this->assertContains($joke, $chuckNorrisJokes);
